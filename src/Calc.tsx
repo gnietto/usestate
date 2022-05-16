0b29954bf1567ba575191341e5d6c8ca8c1e99e2
workstation <elcirculoti@gmail.com>
2022-05-16 18:11:17 -0400

    Agrega teclas y sus colores; centrado horizontal de calculadora

--- src/Calc.tsx
+++ src/Calc.tsx
@@ -1,32 +1,81 @@
 import { useState } from 'react';
-import { Box, Input, Button } from '@chakra-ui/react';
+import { Box, SimpleGrid, Input, Button } from '@chakra-ui/react';
 
 function Calculadora() {
   const [visor, setVisor] = useState('');
+
   const despliegueEnVisor = (e: any) =>
     setVisor(visor + e.target.value.toString());
+
   const resultado = () => setVisor(eval(visor));
+
   const limpiarVisor = () => setVisor('');
 
   return (
-    <Box>
+    <Box maxW="780px" my="0" mx="auto">
       <Input value={visor} onChange={despliegueEnVisor} type="text" readOnly />
-      <Button value="1" onClick={despliegueEnVisor}>
-        1
-      </Button>
-      <Button value="2" onClick={despliegueEnVisor}>
-        2
-      </Button>
-      <Button value="+" onClick={despliegueEnVisor}>
-        +
-      </Button>
-      <Button value="=" onClick={resultado}>
-        =
-      </Button>
-      <Button value="C" onClick={limpiarVisor}>
-        {' '}
-        C{' '}
-      </Button>
+      <SimpleGrid columns={4} spacing={1}>
+        <Button value="C" colorScheme="blackAlpha" onClick={limpiarVisor}>
+          C
+        </Button>
+        <Button value="(" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          (
+        </Button>
+        <Button value=")" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          )
+        </Button>
+        <Button value="/" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          /
+        </Button>
+        <Button value="7" onClick={despliegueEnVisor}>
+          7
+        </Button>{' '}
+        <Button value="8" onClick={despliegueEnVisor}>
+          8
+        </Button>{' '}
+        <Button value="9" onClick={despliegueEnVisor}>
+          9
+        </Button>{' '}
+        <Button value="*" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          *
+        </Button>
+        <Button value="4" onClick={despliegueEnVisor}>
+          4
+        </Button>{' '}
+        <Button value="5" onClick={despliegueEnVisor}>
+          5
+        </Button>{' '}
+        <Button value="6" onClick={despliegueEnVisor}>
+          6
+        </Button>{' '}
+        <Button value="-" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          -
+        </Button>{' '}
+        <Button value="1" onClick={despliegueEnVisor}>
+          1
+        </Button>
+        <Button value="2" onClick={despliegueEnVisor}>
+          2
+        </Button>{' '}
+        <Button value="3" onClick={despliegueEnVisor}>
+          3
+        </Button>{' '}
+        <Button value="+" colorScheme="blackAlpha" onClick={despliegueEnVisor}>
+          +
+        </Button>{' '}
+        <Button value="." onClick={despliegueEnVisor}>
+          .
+        </Button>
+        <Button value="0" onClick={despliegueEnVisor}>
+          0
+        </Button>{' '}
+        <Button value="**" onClick={despliegueEnVisor}>
+          Exp
+        </Button>
+        <Button value="=" colorScheme="whatsapp" onClick={resultado}>
+          =
+        </Button>
+      </SimpleGrid>
     </Box>
   );
 }
