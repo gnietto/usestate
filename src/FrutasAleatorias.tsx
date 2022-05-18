0fc6b897a049e9aafa8915d38d93b8108c13da21
workstation <elcirculoti@gmail.com>
2022-05-17 21:31:58 -0400

    Dispone imágenes de frutas triangularmente

--- src/FrutasAleatorias.tsx
+++ src/FrutasAleatorias.tsx
@@ -1,11 +1,15 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Flex, Image } from '@chakra-ui/react';
 
 export default function FrutasAleatorias(props: any) {
   return (
     <Box>
-      <Image src={props.fruta1} w={props.anchoFrutas} />
-      <Image src={props.fruta2} w={props.anchoFrutas} />
-      <Image src={props.fruta3} w={props.anchoFrutas} />
+      <Flex>
+        <Image src={props.fruta1} w={props.anchoFrutas} />
+        <Image src={props.fruta2} w={props.anchoFrutas} />
+      </Flex>
+      <Flex justifyContent="center">
+        <Image src={props.fruta3} w={props.anchoFrutas} />
+      </Flex>
     </Box>
   );
 }
